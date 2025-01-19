ae7de3907e0b64fa68077825fd77805048b40cb6
MannyOnBrazzers <mannygraphicdesign@gmail.com>
2025-01-19 06:04:23 -0500

    feat(web): project data added

--- config.js
+++ config.js
@@ -41,8 +41,8 @@ const config = {
   },
   stats: [
     { label: "Years of Experience", value: 5 },
-    { label: "Projects Completed", value: 15 },
-    { label: "Technical Skills", value: 8 },
+    { label: "Projects Completed", value: 6 },
+    { label: "Technical Skills", value: 13 },
     { label: "Happy Clients", value: 10 },
   ],
   skills: [
@@ -62,63 +62,174 @@ const config = {
   ],
   projects: {
     stats: [
-      { label: "Projects completed", image: "./images/fire.svg", value: 5 },
-      { label: "Cup of coffee", image: "./images/coffee.svg", value: 5 },
-      { label: "Liked projects", image: "./images/heart-white.svg", value: 5 },
+      { label: "Projects completed", image: "./images/fire.svg", value: 6 },
+      { label: "Cup of coffee", image: "./images/coffee.svg", value: 8734 },
+      { label: "Liked projects", image: "./images/heart-white.svg", value: 6 },
     ],
     work: [
       {
-        name: "Test Title",
-        description: `The app's user interface is sleek and modern, designed
-          with a flexbox layout to ensure alignment and
-          responsiveness across various screen sizes. Key features
-          include:`,
-        features: ["Contains a lot of things", "Ton of things you'd like"],
-        images: [
-          "./images/project_placeholder.png",
-          "./images/project_placeholder.png",
-          "./images/project_placeholder.png",
-          "./images/project_placeholder.png",
-          "./images/project_placeholder.png",
-          "./images/project_placeholder.png",
-          "./images/project_placeholder.png",
-          "./images/project_placeholder.png",
+        name: "Radar System",
+        description: `Radar & Lidar system including a settings menu made for the CFX platform. Settings allows full control of the theme of the radar, and full scalability and position of all UI's on the clients screen. This entire system retrieves data from the CFX platform using Lua`,
+        features: [
+          "Complete UI control, allowing the user to move around all UI's, change scale, and alter themes. All changes are saved and will never require you to do it again.",
         ],
+        images: ["./images/radar/radar.png"],
         links: [
-          { label: "Video Demo", url: "https://www.google.com/" },
-          { label: "GitHub", url: "https://www.google.com/" },
+          {
+            label: "GitHub",
+            url: "https://github.com/Brazzers-Development/brazzers_radar",
+          },
         ],
-        tags: ["Web Design", "Application"],
+        tags: ["UI/UX Design", "Private"],
         languages: [
-          { name: "HTML", percentage: 50 },
-          { name: "JavaScript", percentage: 10 },
+          { name: "Lua", percentage: 52.7 },
+          { name: "JavaScript", percentage: 20.7 },
+          { name: "CSS", percentage: 15.7 },
+          { name: "HTML", percentage: 10.9 },
         ],
       },
       {
-        name: "Test Title 2",
+        name: "Business Application",
         description: `The app's user interface is sleek and modern, designed
           with a flexbox layout to ensure alignment and
-          responsiveness across various screen sizes. Key features
-          include:`,
-        features: ["Contains a lot of things", "Ton of things you'd like"],
+          responsiveness across various screen sizes. The intended system is to be tied
+          into the CFX platform utilizing Lua to retrieve the data, but can be used for many
+          different cases.`,
+        features: [
+          "Sleek and modern design",
+          "Role Management",
+          "Employee Management",
+        ],
+        images: ["./images/phone-app/phone-app-employment.png"],
+        links: [
+          {
+            label: "GitHub",
+            url: "https://github.com/Rush-Hour-Roleplay/phone_business_app",
+          },
+        ],
+        tags: ["UI/UX Design", "Phone App", "Private"],
+        languages: [
+          { name: "JavaScript", percentage: 49.4 },
+          { name: "CSS", percentage: 35.0 },
+          { name: "HTML", percentage: 15.6 },
+        ],
+      },
+      {
+        name: "Laptop",
+        description: `This project is a sophisticated and fully functional laptop system developed using JavaScript, HTML, and CSS. The system offers a dynamic and interactive user experience, featuring 16 unique applications that simulate real-world laptop functionalities. Each application is seamlessly integrated into the system, providing a cohesive and intuitive interface.`,
+        features: [
+          "Comprehensive App Suite",
+          "Modern UI/UX Design",
+          "JavaScript-Powered Interactivity",
+          "App Diversity",
+        ],
         images: [
-          "./images/project_placeholder.png",
-          "./images/project_placeholder.png",
-          "./images/project_placeholder.png",
-          "./images/project_placeholder.png",
-          "./images/project_placeholder.png",
-          "./images/project_placeholder.png",
-          "./images/project_placeholder.png",
-          "./images/project_placeholder.png",
+          "./images/laptop/laptop1.png",
+          "./images/laptop/laptop2.png",
+          "./images/laptop/laptop3.png",
+          "./images/laptop/laptop4.png",
+          "./images/laptop/laptop5.png",
+          "./images/laptop/laptop6.png",
+          "./images/laptop/laptop7.png",
+          "./images/laptop/laptop8.png",
+          "./images/laptop/laptop9.png",
+          "./images/laptop/laptop10.png",
+          "./images/laptop/laptop11.png",
+          "./images/laptop/laptop12.png",
+          "./images/laptop/laptop13.png",
+          "./images/laptop/laptop14.png",
+          "./images/laptop/laptop15.png",
+          "./images/laptop/laptop16.png",
+          "./images/laptop/laptop17.png",
+          "./images/laptop/laptop18.png",
+          "./images/laptop/laptop19.png",
+          "./images/laptop/laptop20.png",
+          "./images/laptop/laptop21.png",
+          "./images/laptop/laptop22.png",
         ],
         links: [
-          { label: "Video Demo", url: "https://www.google.com/" },
-          { label: "GitHub", url: "https://www.google.com/" },
+          {
+            label: "GitHub",
+            url: "https://github.com/Rush-Hour-Roleplay/Laptop",
+          },
         ],
-        tags: ["Fucking shitter"],
+        tags: ["Application", "UI/UX Design", "Private"],
         languages: [
-          { name: "HTML", percentage: 50 },
-          { name: "JavaScript", percentage: 10 },
+          { name: "JavaScript", percentage: 37.0 },
+          { name: "CSS", percentage: 34.9 },
+          { name: "HTML", percentage: 28.1 },
+        ],
+      },
+      {
+        name: "Nexus Menu",
+        description: `This project is a comprehensive game User Interface (UI) developed using JavaScript, HTML, and CSS. The UI integrates seamlessly with the game, offering players an intuitive and visually engaging experience. It is designed to enhance player interaction through a skill system, mission engagement interface, emote selection menu, and a robust settings menu.`,
+        features: [
+          "Dynamic Content",
+          "Polished Design",
+          "Modular Architecture",
+          "Smooth Animations",
+        ],
+        images: [
+          "./images/nexus/nexus1.png",
+          "./images/nexus/nexus2.png",
+          "./images/nexus/nexus3.png",
+          "./images/nexus/nexus4.png",
+        ],
+        links: [
+          {
+            label: "GitHub",
+            url: "https://github.com/Rush-Hour-Roleplay/specialization_menu",
+          },
+        ],
+        tags: ["Game Design", "UI/UX Design", "Private"],
+        languages: [
+          { name: "JavaScript", percentage: 41.0 },
+          { name: "CSS", percentage: 37.8 },
+          { name: "HTML", percentage: 21.2 },
+        ],
+      },
+      {
+        name: "HUD",
+        description: `This project is a highly functional and visually engaging Heads-Up Display (HUD) developed using JavaScript, HTML, and CSS for an immersive in-game experience. The HUD is designed to provide real-time feedback to players, combining critical gameplay elements such as a vehicle dashboard, a map HUD, and a directional compass into a seamless interface.`,
+        features: [
+          "Real-Time Feedback",
+          "Responsive Design",
+          "Smooth Animations",
+          "Immersive Integration",
+          "Clutter-Free Layout",
+        ],
+        images: ["./images/hud/hud.png"],
+        links: [
+          { label: "GitHub", url: "https://github.com/Rush-Hour-Roleplay/hud" },
+        ],
+        tags: ["Game Design", "UI/UX  Design", "Private"],
+        languages: [
+          { name: "HTML", percentage: 40.0 },
+          { name: "JavaScript", percentage: 38.4 },
+          { name: "CSS", percentage: 21.6 },
+        ],
+      },
+      {
+        name: "Character Customization",
+        description: `This project is a fully interactive clothing menu developed using JavaScript, HTML, and CSS, designed to enhance in-game character customization. The menu allows players to personalize their characters with a variety of clothing options, accessories, and styles. With a sleek and responsive interface, the clothing menu provides a user-friendly experience while integrating seamlessly with the game environment.`,
+        features: [
+          "Preview System",
+          "Sliders",
+          "Toggle Buttons",
+          "Color Palette",
+        ],
+        images: ["./images/clothing/clothing.png"],
+        links: [
+          {
+            label: "GitHub",
+            url: "https://github.com/Rush-Hour-Roleplay/clothing_menu",
+          },
+        ],
+        tags: ["Game Design", "UI/UX  Design", "Private"],
+        languages: [
+          { name: "HTML", percentage: 84.7 },
+          { name: "JavaScript", percentage: 8.8 },
+          { name: "CSS", percentage: 6.5 },
         ],
       },
     ],
